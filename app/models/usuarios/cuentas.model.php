3741a611d3abbd0d7d90a565ca99d60e4d3fca55
gilverduzco@gmail.com <gilverduzco@gmail.com>
2019-02-24 13:59:39 -0700

    correccion

--- app/models/usuarios/cuentas.model.php
+++ app/models/usuarios/cuentas.model.php
@@ -1,8 +1,7 @@
 <?php
     class CuentasModel extends BaseModel {
          //Método para asociar cuentas.
-        public function agregar( $params ) {
-            var_dump( $params ); die(); 
+        public function agregar( $params ) { 
             return $this->connection->insert('cuentas:id_cuenta', [
                 '_insert' => $params
             ]);
